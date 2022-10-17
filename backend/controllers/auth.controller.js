be58f1fb920acc94de3f2543cb99bf4bfc3589f3
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-10-17 14:31:18 +0300

    Backend <> auth controller: fix upload image

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -33,7 +33,7 @@ const signup = async(req,res)=>{
         user.profile_url = profile_url;
         user.user_type = user_type;
         await user.save();
-        image_decoder.writeFile(profile_url, image_base64, 'base64', (err)=> {
+        image_decoder.writeFileSync(profile_url, image_base64+"", 'base64', (err)=> {
                 console.log("user added");
               });
 
